5e5a04c74bc9c31dc8ce58df42c09ab999206ad5
Charlie Paull <charliepaull211@gmail.com>
2019-08-08 16:32:14 -0700

    orm setup done with hibernatejs

--- connection.js
+++ connection.js
@@ -2,53 +2,73 @@
 
 //  requiring mysql
 let mysql = require("mysql");
-// requiring hibernate ORM mapper
-let jsORM = require('js-hibernate');
-// create a session
-let session = jsORM.session(connection);
 
 // config connection to database
-let connection = {
+let connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "baseball_players"
-};
+});
+
+// requiring hibernate ORM mapper
+let jsORM = require('js-hibernate');
+// create a session
+let session = jsORM.session(connection);
+
+
+// call connect() method on connection object - return err if err, return console.log of success if success
+connection.connect(err => {
+    if (err) {
+      console.error("error connecting" + err.stack);
+      return;
+    }
+  
+    console.log("connected to the MySQL server");
+  });
+  
 
 // create a session using session & jsORM globally
 // create the map to look for what we are querying for
 let playerMap = session.tableMap("Player")
     .columnMap("id", "id")
-    .columnMap("player", "player name");
+    .columnMap("player", "playerName");
 
 // orm object
 
-let baseballORM = {
-    
-}
-
 // query to select * from baseball_players
-let allPlayers = session.query(playerMap).select();
-allPlayers
-  .then(function(result) {
-    console.log(result); // array with result
-    // will have to make sure this turns into a res.send w/ JSON
-  })
-  .catch(function(error) {
-    console.log("Error: " + error);
-  });
+const selectAll = (baseball_players) => {
+  let allPlayers = session.query(playerMap).select(baseball_players);
+  allPlayers
+    .then(result => {
+      console.log(result); // array with result
+      // will have to make sure this turns into a res.send w/ JSON
+    })
+    .catch(error => {
+      console.log("Error: " + error);
+    });
+};
 
 
+// inserting players into the DB
+const insertPlayer = (player) => {
+  playerMap = session.tableMap("Player")
+    .columnMap("id", "id", { isAutoIncrement: true })
+    .columnMap("player", "playerName");
 
-// call connect() method on connection object - return err if err, return console.log of success if success
-connection.connect(err => {
-  if (err) {
-    console.error("error connecting" + err.stack);
-    return;
-  }
-
-  console.log("connected to the MySQL server");
-});
+  // this will have to capture the data from the form
+  var newPlayer = {
+    player: player
+  };
+  playerMap
+    .Insert(newPlayer)
+    .then(result => {
+      console.log("inserted: " + result.affectedRows);
+    })
+    .catch(error => {
+      console.log("Error: " + error);
+    });
+};
 
 module.exports = connection;
